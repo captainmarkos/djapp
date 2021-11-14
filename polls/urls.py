4c91f7b7df7a78f986e7492468a3a5bdad649c0e
Mark Brettin <mark.brettin@dadesystems.com>
2021-11-14 13:07:20 -0700

    trying to get the routes right

--- polls/urls.py
+++ polls/urls.py
@@ -1,12 +1,10 @@
-from django.urls import include, path
+from django.urls import path
 
 from . import views
 
-app_name = 'polls'
+#app_name = 'polls'
 
 urlpatterns = [
-   path('admin/', admin.site.urls),
-
    # ex: /polls/
    path('', views.index, name='index'),
 
@@ -14,8 +12,8 @@ urlpatterns = [
    path('<int:question_id>/', views.detail, name='detail'),
 
    # ex: /polls/5/results/
-   path('<int:question_id>/results/', views.results, name='results'),
+   #path('<int:question_id>/results/', views.results, name='results'),
 
    # ex: /polls/5/vote/
-   path('<int:question_id>/vote/', views.vote, name='vote'),
+   #path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
